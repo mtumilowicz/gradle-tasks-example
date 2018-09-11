a36f1640a21284f75c040b071e39059ad208ffdc
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-09-11 18:23:32 +0200

    Initial commit

---
+++ src/main/java/app/App.java
@@ -0,0 +1,10 @@
+package app;
+
+/**
+ * Created by mtumilowicz on 2018-09-10.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
